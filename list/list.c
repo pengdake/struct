1a2d4b1a6bf13138673d279c529c48923f1ad5cc
pengdake <19921207pq@gmail.com>
2017-03-07 22:57:02 +0800

    add list

--- list/list.c
+++ list/list.c
@@ -16,6 +16,7 @@ Position Find(ElementType X, List L)
 {
 	Position P;
 	
+	P = L->Next;
 	while((P != NULL) && (P->Element != X))
 	{
 		P = P->Next;
@@ -24,15 +25,41 @@ Position Find(ElementType X, List L)
 }
 void Delete(ElementType X, List L)
 {
+	Position P, TmpCell;
 
+	P = FindPrevious(X, L);
+	if(!IsLast(P, L))
+	{
+		TmpCell = P->Next;
+		P->Next = TmpCell->Next;
+		free(TmpCell);
+	}
+	
 }
 Position FindPrevious(ElementType X, List L)
 {
+	Position P;
+
+	P = L;
+	while((P->Next != NULL)&&(P->Next->Element != x ))
+	{
+		P = P->Next;
+	}
+	return P;
 
 }
 void Insert(ElementType X, List L, Position P)
 {
-
+	Position TmpCell;
+	
+	TmpCell = malloc(sizeof(Struct Node));
+        if(TmpCell == NULL)
+	{
+		FatalError("out of space");
+	}
+	TmpCell->Next = P->Next;
+	TmpCell->Element = X;
+	P->Next = TmpCell;
 }
 void DeleteList(List L)
 {
