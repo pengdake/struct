7080b8a7e6dce1738b67c9e9a90d0a05ab8044eb
pengdake <19921207pq@gmail.com>
2017-03-11 07:10:32 +0800

    add 3.19

---
+++ stack/3.19.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include 'stack.h'
+#define MAXLINE 30;
+int compute(Stack S)
+{
+	char arr[MAXLINE];
+	int i, ele_1, ele_2, result;
+        char symbol[] = {'+', '-', '*', '/'};
+
+	result = 0
+        while((s = fgets(arr, MAXLINE, stdin)) != EOF)
+	{
+		len = 4
+        	for(i = 0;i<len;i++ )
+		{
+			if(s == symbol[i])
+			{
+	            		ele_1 = Top(S);
+				Pop(S);
+				ele_2 = Top(S);
+				pop(S);
+                                result = ele_1
+			}
+			else
+			{
+				
+			}
+	}	
+        
+}
